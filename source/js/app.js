81df5d4e5fd9eb921550d1ea159b65d630134107
rav <korzewski@gmail.com>
2015-10-26 22:43:51 +0100

    added basic message sender

--- source/js/app.js
+++ source/js/app.js
@@ -8,8 +8,9 @@ class Manager extends React.Component{
 		super();
 		this.state = {
 			initChat: false,
-			allConnectedPeers: [],
-			targetUser: ''
+			allConnectedUsers: [],
+			connectedUserName: '',
+			currentMessages: []
 		};
 
 		io.on('connect', () => {
@@ -25,7 +26,7 @@ class Manager extends React.Component{
 
 			io.on(EventsType.ALL_CONNECTED_PEERS_LIST, (allUsers) => {
 				console.log('ALL_CONNECTED_PEERS_LIST: ', allUsers);
-				this.setState({ allConnectedPeers: allUsers })
+				this.setState({ allConnectedUsers: allUsers })
 			});
 		});
 	}
@@ -51,9 +52,55 @@ class Manager extends React.Component{
 	_connectToServer(){
 		this.peer = new Peer(this.userName, { host: location.hostname, port: 9000, path: '/chat'});
 
-		this.peer.on('connection', function(conn){
-			console.log('connection: ', conn);
-			conn.send('test message!');
+		this.peer.on('connection', (conn) => {
+			// console.log('connection: ', conn);
+			conn.on('open', () => {
+				console.log('on connection -> open: ', conn);
+
+				conn.on('data', (data) => {
+					// console.log('on data -> data: ', data);
+					this.getMessage(data);
+				})
+			});
+		});
+	}
+
+
+
+	changeConnectedUser(peerID){
+		console.log('change user: ', peerID);
+
+		var conn = this.peer.connect(peerID);
+		conn.on('open', () => {
+			console.log('on open -> send message');
+			this.connectedPeer = conn;
+
+			this.setState({
+				connectedUserName: conn.peer
+			});
+			console.log('connectedUserName: ', this.state.connectedUserName);
+
+
+			// Send messages
+			// conn.send('Hello!');
+			this.sendMessage('hi how are you!?');
+		});
+	}
+
+	sendMessage(message){
+		console.log('sendMessage: ', message);
+		this.connectedPeer.send({
+			userName: this.userName,
+			message: message
+		});
+	}
+
+	getMessage(data){
+		console.log('getMessage: ', data);
+		var messages = this.state.currentMessages;
+		messages.push(data.message);
+		this.setState({
+			currentMessages: messages
 		});
 	}
 }
@@ -61,22 +108,11 @@ class Manager extends React.Component{
 
 class ChatApp extends Manager{
 
-	changeTargetUser(peerID){
-		console.log('change user: ', peerID);
-		var conn = this.peer.connect(peerID, {
-			serialization: 'none'
-		});
-		conn.on('open', function() {
-			console.log('on open');
-			
-			// Receive messages
-			conn.on('data', function(data) {
-				console.log('Received', data);
-			});			
-
-			// Send messages
-			conn.send('Hello!');
-		});
+	
+	sendMessageHandle(){
+		// console.log('ref: ', this.refs.messageInput.value);
+		this.sendMessage(this.refs.messageInput.value);
+		this.refs.messageInput.value = '';
 	}
 
 	render(){
@@ -86,19 +122,19 @@ class ChatApp extends Manager{
 					<div className="row">
 						<div className="col-sm-4 users-list">
 							<h2>Connected users</h2>
-							<UsersList users={ this.state.allConnectedPeers } changeTargetUser={ (peerID) => this.changeTargetUser(peerID) } />
+							<UsersList user={ this.userName } users={ this.state.allConnectedUsers } changeConnectedUser={ (peerID) => this.changeConnectedUser(peerID) } />
 						</div>
 						<div className="col-sm-8 messages-list">
 							<div className="col-sm-12">
 								<h2>Messages</h2>
-								<MessagesList userName={ this.userName } targetUser={ this.state.targetUser } />
+								<MessagesList userName={ this.userName } connectedUserName={ this.state.connectedUserName } messages={ this.state.currentMessages } />
 							</div>
 						</div>
 					</div>
 					<div className="row">
 						<div className="col-sm-8 user-input">
-							<input className="col-sm-8" type="text" />
-							<button className="btn btn-primary">Send message</button>
+							<input ref="messageInput" className="col-sm-8" type="text" />
+							<button className="btn btn-primary" onClick={ () => this.sendMessageHandle() }>Send message</button>
 						</div>
 					</div>
 				</div>
@@ -114,15 +150,19 @@ class ChatApp extends Manager{
 }
 
 class UsersList extends React.Component{
-	changeTargetUser(peerID){
-		this.props.changeTargetUser(peerID);
+	changeConnectedUser(peerID){
+		this.props.changeConnectedUser(peerID);
 	}
 
 	render(){
 		var listItems = [];
 		if(typeof this.props.users != 'undefined'){
 			this.props.users.forEach((user, index) => {
-				listItems.push(<li key={ index } onClick={ () => this.changeTargetUser(user.peerID) }>{ user.peerID }</li>);		
+				if(user.peerID == this.props.user){
+					listItems.push(<li key={ index } onClick={ () => this.changeConnectedUser(user.peerID) } className="current-user">your name: { user.peerID }</li>);	
+				} else {
+					listItems.push(<li key={ index } onClick={ () => this.changeConnectedUser(user.peerID) }>{ user.peerID }</li>);	
+				}
 			})
 		}
 		
@@ -136,11 +176,21 @@ class UsersList extends React.Component{
 
 class MessagesList extends React.Component{
 	render(){
+		var messages = [];
+		console.log('this.props.messages: ', this.props.messages);
+		if(typeof this.props.messages != 'undefined'){
+			this.props.messages.forEach((message, index) => {
+				messages.push(
+					<li key={ index }><span className="user-name">{ this.props.connectedUserName }: </span>{ message }</li>
+				)
+			});
+		}
+		
 		return (
 			<div>
-				<h4>Hello { this.props.userName }, you writting to: { this.props.targetUser }</h4>
+				<h4>Hello { this.props.userName }</h4>
 				<ul>
-					<li></li>
+					{ messages }
 				</ul>
 			</div>
 		)
