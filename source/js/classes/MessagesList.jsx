605b087b48889790b5ac17f8f07dcf14fa59876e
rav <korzewski@gmail.com>
2015-11-02 23:11:37 +0100

    added boldFilter for messages when user types *

--- source/js/classes/MessagesList.jsx
+++ source/js/classes/MessagesList.jsx
@@ -1,14 +1,43 @@
 import React from 'react';
 
 export default class MessagesList extends React.Component{
+
+    boldFilter(text){
+        if(text.indexOf('*') !== -1){
+            var textArray = text.split('*');
+            textArray.forEach((string, index) => {
+                if( index % 2 && string.length ){
+                    textArray[index] = '<strong>' + string + '</strong>';
+                }
+            });
+            var formattedText = textArray.join('');
+            return formattedText;
+        }
+
+        return text;
+    }
+
     render(){
         var messages = [];
         if(typeof this.props.messages != 'undefined'){
             this.props.messages.data.forEach((messageData, index) => {
+
+                var message = this.boldFilter(messageData.msg);
+
                 if(messageData.isSelfMessage){
-                    messages.push(<li key={ index } className="self-message"><span className="user-name">you: </span>{ messageData.msg }</li>)
+                    messages.push(
+                        <li key={ index } className="self-message">
+                            <span className="user-name">you: </span>
+                            <span dangerouslySetInnerHTML={{__html: message}} />
+                        </li>
+                    );
                 } else {
-                    messages.push(<li key={ index }><span className="user-name">{ this.props.connectedUserName }: </span>{ messageData.msg }</li>)
+                    messages.push(
+                        <li key={ index }>
+                            <span className="user-name">{ this.props.connectedUserName }: </span>
+                            <span dangerouslySetInnerHTML={{__html: message}} />
+                        </li>
+                    );
                 }
             });
         }
