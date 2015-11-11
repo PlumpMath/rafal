e4ba13c1cbec416eaf0f57a8ef7d769c53d1f159
rav <korzewski@gmail.com>
2015-11-11 08:29:45 +0100

    fixed server disconnect bug

--- server.js
+++ server.js
@@ -8,32 +8,26 @@ app.use(express.static( __dirname + '/build' ));
 var expressServer = app.listen(3001);
 var io = require('socket.io').listen(expressServer);
 
-
 var peerServer = new PeerServer({ port: 9000, path: '/chat' });
+var allConnectedPeers = [];
 
 peerServer.on('connection', function(peerID){
 	allConnectedPeers.push( { peerID: peerID } );
-
 	io.emit(EventsType.USER_CONNECTED, peerID);
-	// io.emit(EventsType.ALL_CONNECTED_PEERS_LIST, allConnectedPeers);
 
 	console.log('new user connected: ', peerID);
 });
 
-peerServer.on('disconnect', function(peerID, index){
-	allConnectedPeers.forEach((peer) =>{
+peerServer.on('disconnect', function(peerID){
+	allConnectedPeers.forEach((peer, index) =>{
 		if(peer.peerID == peerID){
 			allConnectedPeers.splice(index, 1);
 		}
 	});
-
 	io.emit(EventsType.USER_DISCONNECTED, peerID);
 
-	console.log('disconnect allConnectedPeers: ', allConnectedPeers);
+	console.log('user disconnect: ', peerID);
 });
-
-var allConnectedPeers = [];
-
 
 app.get('/api/allConnectedPeers', function(req, res){
 	return res.json(allConnectedPeers);
